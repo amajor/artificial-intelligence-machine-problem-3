425053f022240dee55335317044c8437510bd75c
Alison Major <alison.major@gmail.com>
2021-10-05 21:58:29 -0400

    Create test for map_to_term_label()

--- test_mp3.py
+++ test_mp3.py
@@ -1,5 +1,8 @@
 """ The unit tests for the GenGameBoard class. """
 import unittest
+from parameterized import parameterized
+
+import mp3
 
 
 class TestCoursePlanning(unittest.TestCase):
@@ -9,9 +12,20 @@ class TestCoursePlanning(unittest.TestCase):
         """ Tests the creation of a list of term indexes for years in the future. """
         self.skipTest('Test not yet created.')
 
-    def test_map_to_term_label(self):
+    @parameterized.expand([
+        ('Term number 0 is "Not Taken"', 0, 'Not Taken'),
+        ('Term number 1 is "Year 1 Fall 1"', 1, 'Year 1 Fall 1'),
+        ('Term number 2 is "Year 1 Fall 2"', 2, 'Year 1 Fall 2'),
+        ('Term number 3 is "Year 1 Spring 1"', 3, 'Year 1 Spring 1'),
+        ('Term number 4 is "Year 1 Spring 2"', 4, 'Year 1 Spring 2'),
+        ('Term number 5 is "Year 1 Summer 1"', 5, 'Year 1 Summer 1'),
+        ('Term number 6 is "Year 1 Summer 2"', 6, 'Year 1 Summer 2'),
+        ('Term number 13 is "Year 3 Fall 1"', 13, 'Year 3 Fall 1'),
+    ])
+    def test_map_to_term_label(self, _test_name, term_number, expected_label):
         """ Tests mapping the term number to its label. """
-        self.skipTest('Test not yet created.')
+        actual_label = mp3.map_to_term_label(term_number)
+        self.assertEqual(expected_label, actual_label)
 
     def test_pre_requisite(self):
         """ Tests the limitations constraints for prerequisite courses. """
